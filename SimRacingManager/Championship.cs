05a0c2405468b6164ba09f224a26ff6b98b61e4c
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-05-09 18:38:52 +0930

    Very awful way to grab the instance of a championship that is passed across

--- SimRacingManager/Championship.cs
+++ SimRacingManager/Championship.cs
@@ -1,6 +1,23 @@
 ﻿namespace SimRacingManager;
 
-public class Championship(string name)
+public class Championship
 {
-    public string Name = $"{name} Championship";
+    public string Name;
+    public Guid GUID;
+    
+    public Championship(string name)
+    {
+        Name = $"{name} Championship";
+        GUID = Guid.NewGuid();
+    }
+
+    public Championship GrabInstanceFromGUID(Guid guid)
+    {
+        if (GUID == guid)
+        { 
+            return this;
+        }
+
+        return null;
+    }
 }
