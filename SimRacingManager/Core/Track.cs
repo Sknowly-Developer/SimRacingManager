eeee5163d030ec876c3721edda52f26161a5384a
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-05-11 12:36:16 +0930

    Championships are now also dynamically added to HTML

--- SimRacingManager/Core/Track.cs
+++ SimRacingManager/Core/Track.cs
@@ -3,13 +3,11 @@
 public class Track
 {
     public string Name;
-    public string ImageSource;
     public List<Track>? Layouts;
 
-    public Track(string name, string imageSource, List<Track>? layouts = null)
+    public Track(string name, List<Track>? layouts = null)
     {
         Name = name;
-        ImageSource = imageSource;
         Layouts = layouts;
     }
 }
--- SimRacingManager/Data/Tracks.cs
+++ SimRacingManager/Data/Tracks.cs
@@ -4,14 +4,14 @@ namespace SimRacingManager.Data;
 
 public static class Tracks
 {
-    public static Track RoadAmerica = new Track("Road America", "", [new Track("Road America (Bend)", ""), new Track("Road America STT", "")]);
-    public static Track WatkinsGlen = new Track("Watkins Glen", "");
-    public static Track VirginiaInternationalRaceway = new Track("Virginia International Raceway", "");
-    public static Track Sebring = new Track("Sebring", "");
-    public static Track LagunaSeca = new Track("", "");
-    public static Track Spielberg = new Track("", "");
-    public static Track JerezChicane = new Track("", "");
-    public static Track NurburgringGP = new Track("", "");
-    public static Track Cascais = new Track("", "");
-    public static Track Hockenheimring = new Track("", "");
+    public static Track RoadAmerica = new("Road America",  [new Track("Road America (Bend)"), new Track("Road America STT")]);
+    public static Track WatkinsGlen = new("Watkins Glen");
+    public static Track VirginiaInternationalRaceway = new("Virginia International Raceway");
+    public static Track Sebring = new("Sebring");
+    public static Track LagunaSeca = new("");
+    public static Track Spielberg = new("");
+    public static Track JerezChicane = new("");
+    public static Track NurburgringGP = new("");
+    public static Track Cascais = new("");
+    public static Track Hockenheimring = new("");
 } 
