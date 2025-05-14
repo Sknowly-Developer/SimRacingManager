05d3966a20904d7ce39982a0138760eaf0067d11
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-05-14 19:12:19 +0930

    Added start and end dates to Championships.cs

--- SimRacingManager/Core/Championship.cs
+++ SimRacingManager/Core/Championship.cs
@@ -6,18 +6,23 @@ public class Championship
 {
     public string Name;
     public int Year;
+    public DateTime StartingDate;
+    public DateTime EndDate;
+    public string CombinedDates;
+    public int TracksCompleted;
+    public Driver Winner;
     public Status Status;
     public MudBlazor.Color StatusColour;
     public Guid Guid;
     public List<Driver> Drivers;
     public List<TrackBase> Tracks;
-    public int TrackCompleted;
-    public Driver Winner;
     
-    public Championship(string name, int year, Status status, List<Driver> drivers, List<TrackBase> tracks, Driver winner = null)
+    public Championship(string name, int year, DateTime startingDate, DateTime endDate, Status status, List<Driver> drivers, List<TrackBase> tracks, Driver winner = null)
     {
         Name = $"{name} Championship";
         Year = year;
+        StartingDate = startingDate;
+        EndDate = endDate;
         Status = status;
         Guid = Guid.NewGuid();
         Drivers = drivers;
@@ -28,9 +33,9 @@ public class Championship
     /// <summary>
     /// Loop through all the tracks and count which ones have been completed.
     /// </summary>
-    public void TracksCompleted()
+    public void TracksCompletedCount()
     {
-        if (TrackCompleted > 0) // Adding this conditional in case the method is called more than once.
+        if (TracksCompleted > 0) // Adding this conditional in case the method is called more than once.
         {
             return;
         }
@@ -39,8 +44,13 @@ public class Championship
         {
             if (track.Status == Status.Completed)
             {
-                TrackCompleted += 1;
+                TracksCompleted += 1;
             }
         }
+    }
+
+    public void CombineDates()
+    {
+        CombinedDates = $"{StartingDate.ToShortDateString()} to {EndDate.Date.ToShortDateString()}";
     }
 }
--- SimRacingManager/Data/Championships.cs
+++ SimRacingManager/Data/Championships.cs
@@ -6,12 +6,12 @@ namespace SimRacingManager.Data;
 
 public static class Championships
 {
-    private static Championship Y2025Q1 = new("Quarter 1", 2025, Status.Completed, 
+    private static Championship Y2025Q1 = new("Quarter 1", 2025, new DateTime(2025, 2, 5), new DateTime(2025, 4, 3), Status.Completed, 
         [Drivers.Adam, Drivers.Damien, Drivers.Harvey, Drivers.Lee, Drivers.Matthew, Drivers.Trent], 
         [new BrandsHatch(Status.Completed), new DoningtonPark(Status.Completed), new OultonPark(Status.Completed), new Kyalami(Status.Completed), new Interlagos(Status.Completed), new Adelaide(Status.Completed), new Bathurst(Status.Completed)], 
         Drivers.Trent);
     
-    private static Championship Y2025Q2 = new("Quarter 2", 2025, Status.Ongoing, 
+    private static Championship Y2025Q2 = new("Quarter 2", 2025, new DateTime(2025, 4, 16), new DateTime(2025, 6, 25), Status.Ongoing, 
         [Drivers.Adam, Drivers.Damien, Drivers.Harvey, Drivers.Lee, Drivers.Matthew, Drivers.Trent], 
         [new RoadAmerica(Status.Completed), new WatkinsGlen(Status.Completed), new Virginia(Status.Completed), new Sebring(Status.Completed), new LagunaSeca(Status.Upcoming), new Spielberg(Status.Upcoming), new Jerez(Status.Upcoming), new Nurburgring(Status.Upcoming), new Cascais(Status.Upcoming), new Hockenheimring(Status.Upcoming)]);
 
