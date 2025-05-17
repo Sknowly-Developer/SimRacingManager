156dc21fb7e4e1d161670cc5ce32e78e470c4760
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-05-17 12:22:18 +0930

    Fixed up the logic for connecting to the database and grabbing data from tables

---
+++ SimRacingManager/Core/DatabaseManager.cs
@@ -0,0 +1,36 @@
+﻿using Supabase;
+
+namespace SimRacingManager.Core;
+
+public class DatabaseManager
+{
+    private const string Url = "https://rxrohdwbxmzoeichstic.supabase.co";
+    private const string Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4cm9oZHdieG16b2VpY2hzdGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDI1MTEsImV4cCI6MjA2Mjg3ODUxMX0.ptcPOyeeS0v4FOwB9rnMthEvyfjh7Q1oA0pZjOZLOjI";
+
+    public static Client SupabaseClient;
+    private static bool _hasAlreadyBeenInitialised;
+    
+    public static async void Initialise()
+    {
+        try
+        {
+            // Once we have a connection, we don't need to keep overriding it.
+            if (!_hasAlreadyBeenInitialised)
+            {
+                var options = new SupabaseOptions
+                {
+                    AutoConnectRealtime = true
+                };
+
+                SupabaseClient = new Client(Url, Key, options);
+                await SupabaseClient.InitializeAsync();   
+            }
+
+            _hasAlreadyBeenInitialised = true;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
+}
