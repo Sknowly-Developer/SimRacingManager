f3e68b23dd7472b5f1c3362011afa6423712ff51
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-06-15 03:41:49 +0930

    Vehicle Type for each Driver are now displaying within each championship and track within that championship

--- SimRacingManager/Data/Globals.cs
+++ SimRacingManager/Data/Globals.cs
@@ -1,6 +1,9 @@
-﻿namespace SimRacingManager.Data;
+﻿using SimRacingManager.Core;
+
+namespace SimRacingManager.Data;
 
 public static class Globals
 {
     public static bool HasDatabaseManagerBeenInitialised;
+    public static Championship LastSelectedChampionship;
 }
